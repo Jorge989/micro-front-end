68d7b98061918dbcf840aadf498ad6c7dd5b722d
Jorge <attiejorge89@gmail.com>
2022-03-24 20:41:56 -0300

    faster

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi th22e222r22222e!!!</h1>
+      <h1>Hi !!!</h1>
       <hr />
       <MarketingApp />
     </div>
