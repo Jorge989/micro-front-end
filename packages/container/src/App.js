ccddb5ab987e77ebd64ec9e8bd3949de4711aeba
Jorge <attiejorge89@gmail.com>
2022-03-24 16:53:37 -0300

    gofor4

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi!22</h1>
+      <h1>Hi!222</h1>
       <hr />
       <MarketingApp />
     </div>
